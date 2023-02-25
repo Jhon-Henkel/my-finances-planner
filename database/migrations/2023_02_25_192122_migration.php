871d5836a56f60bcde54de80474fa26d855094a7
Jhonatan Henkel <jhowrf@gmail.com>
2023-02-25 19:06:31 -0300

    Update 2023_02_25_192122_migration.php

--- database/migrations/2023_02_25_192122_migration.php
+++ database/migrations/2023_02_25_192122_migration.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -15,7 +16,8 @@ return new class extends Migration
             $table->increments('id');
             $table->string('name', '255')->nullable(false)->unique();
             $table->string('value', '255');
-            $table->timestamps();
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
         });
     }
 
