77229db9bf4be3fe86742b8ce536906b9f578662
Jhonatan Henkel <jhowrf@gmail.com>
2023-03-12 19:16:50 -0300

    Movido nav para snippet

---
+++ resources/views/snippets/nav-content.blade.php
@@ -0,0 +1,15 @@
+<nav>
+    <ul class="nav justify-content-end">
+        <li class="nav-item">
+            {{-- todo criar rotaa e telaa para configurações --}}
+            <a class="nav-link active" aria-current="page" href="#">
+                <i class="fa-solid fa-gear"></i>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="{{route('logout')}}">
+                <i class="fa-solid fa-right-from-bracket"></i>
+            </a>
+        </li>
+    </ul>
+</nav>
