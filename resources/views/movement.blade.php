a1aa2f1bf8c22d46a043c91e756d017ec21fbe1b
Jhonatan Henkel <jhowrf@gmail.com>
2023-03-18 22:57:03 -0300

    movido o topButtons da tela de movimentações para um snippet

--- resources/views/movement.blade.php
+++ resources/views/movement.blade.php
@@ -1,39 +1,12 @@
 @php
     use App\Enums\MovementEnum;
-    use App\Enums\RouteEnum;
     use App\Tools\StringTools;
     use App\Tools\CalendarTools;
 @endphp
 @extends('snippets.base')
 @section('content')
     <h3 class="mt-2">Movimentações</h3>
-    <div class="nav justify-content-end">
-        {{-- todo tansformar o filtro em selet --}}
-        <a href="{{ route(RouteEnum::WEB_MOVEMENT, ['filter' => MovementEnum::FILTER_THIS_MONTH]) }}"
-           class="btn btn-info rounded-5 me-2">
-            <i class="fa-solid fa-filter"></i>
-            {{ MovementEnum::getDescription(MovementEnum::FILTER_THIS_MONTH) }}
-        </a>
-        <a href="{{ route(RouteEnum::WEB_MOVEMENT, ['filter' => MovementEnum::FILTER_LAST_MONTH]) }}"
-           class="btn btn-info rounded-5 me-2">
-            <i class="fa-solid fa-filter"></i>
-            {{ MovementEnum::getDescription(MovementEnum::FILTER_LAST_MONTH) }}
-        </a>
-        <a href="{{ route(RouteEnum::WEB_MOVEMENT, ['filter' => MovementEnum::FILTER_THIS_YEAR]) }}"
-           class="btn btn-info rounded-5 me-2">
-            <i class="fa-solid fa-filter"></i>
-            {{ MovementEnum::getDescription(MovementEnum::FILTER_THIS_YEAR) }}
-        </a>
-        <a href="{{ route(RouteEnum::WEB_MOVEMENT, ['filter' => MovementEnum::FILTER_ALL]) }}"
-           class="btn btn-info rounded-5 me-2">
-            <i class="fa-solid fa-filter"></i>
-            {{ MovementEnum::getDescription(MovementEnum::FILTER_ALL) }}
-        </a>
-        <button class="btn btn-success rounded-5" data-bs-toggle="modal" data-bs-target="#insertMovement">
-            <i class="fa-solid fa-money-bill-transfer me-2"></i>
-            Nova Movimentação
-        </button>
-    </div>
+    @include('snippets.movement.topButtons')
     <hr>
     <table class="table table-dark table-striped table-sm table-hover table-bordered" id="dataTable">
         <thead class="table-dark">
