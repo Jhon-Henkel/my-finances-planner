1116ca9c9bffa115dea5f367e3e306617f92f0ad
Jhonatan Henkel <jhowrf@gmail.com>
2023-04-21 16:10:27 -0300

    Finalizado desenvolvimento do InvoiceFactory, atualizado versão de configurações do php unit, ajustado classe de dateEnum

--- app/Factory/InvoiceFactory.php
+++ app/Factory/InvoiceFactory.php
@@ -3,6 +3,7 @@
 namespace App\Factory;
 
 use App\DTO\InvoiceItemDTO;
+use App\Enums\DateEnum;
 use App\Enums\InvoiceEnum;
 use App\VO\InvoiceVO;
 
@@ -20,17 +21,17 @@ class InvoiceFactory
         foreach ($installment as $key => $value) {
             $installments = $expense->getInstallments();
             $totalValue = $expense->getValue() * $installments;
-            $installments = $installments == InvoiceEnum::FIXED_INSTALLMENTS
-                ? InvoiceEnum::MAX_INSTALLMENTS
-                : $installments;
-            if ($thisMonth + $key == $expense->getNextInstallmentMonth()) {
-                $installment[$key] = $expense->getValue();
-                for ($remaining = $key; $remaining <= 5; $remaining ++) {
-                    if (($remaining - $key) + 1 == $installments) {
-                        break;
-                    }
-                    $installment[$remaining + 1] = $expense->getValue();
+            $installments = self::normalizeInstallments($installments);
+            $nextMonth = self::calculateMonthForInstallment($thisMonth, $key);
+            if ($nextMonth != $expense->getNextInstallmentMonth()) {
+                continue;
+            }
+            $installment[$key] = $expense->getValue();
+            for ($remaining = $key; $remaining <= 5; $remaining ++) {
+                if (self::isLimitInstallment($remaining, $key, $installments)) {
+                    break;
                 }
+                $installment[$remaining + 1] = $expense->getValue();
             }
         }
         return InvoiceVO::makeInvoice($expense, $installment, $totalValue);
@@ -46,5 +47,26 @@ class InvoiceFactory
             4 => null,
             5 => null,
         ];
+    }
+
+    protected static function normalizeInstallments(int $installments): int
+    {
+        return $installments == InvoiceEnum::FIXED_INSTALLMENTS
+            ? InvoiceEnum::MAX_INSTALLMENTS
+            : $installments;
+    }
+
+    protected static function calculateMonthForInstallment(int $month, int $sum): int
+    {
+        $nextMonth = $month + $sum;
+        if ($nextMonth > DateEnum::DECEMBER_MONTH_NUMBER) {
+            $nextMonth = $nextMonth - DateEnum::DECEMBER_MONTH_NUMBER;
+        }
+        return $nextMonth;
+    }
+
+    protected static function isLimitInstallment(int $remaining, int $key, int $installments): bool
+    {
+        return ($remaining - $key) + 1 == $installments;
     }
 }
--- tests/Unit/Factory/InvoiceFactoryUnitTest.php
+++ tests/Unit/Factory/InvoiceFactoryUnitTest.php
@@ -4,6 +4,7 @@ namespace Tests\Unit\Factory;
 
 use App\DTO\InvoiceItemDTO;
 use App\Factory\InvoiceFactory;
+use Mockery;
 use Tests\TestCase;
 
 class InvoiceFactoryUnitTest extends TestCase
@@ -246,4 +247,105 @@ class InvoiceFactoryUnitTest extends TestCase
         $this->assertEquals(100.5, $testOne->sixthInstallment);
     }
 
+    public function testFactoryInvoiceWithFixInstallments()
+    {
+        $dtoOne = new InvoiceItemDTO(1, 50, 'Test-one', 100.5, '2023-6', 0);
+        $testOne = InvoiceFactory::factoryInvoice($dtoOne, 1);
+
+        $this->assertEquals(1, $testOne->id);
+        $this->assertEquals(50, $testOne->countId);
+        $this->assertEquals('Test-one', $testOne->name);
+        $this->assertEquals(0, $testOne->totalRemainingValue);
+        $this->assertEquals(null, $testOne->firstInstallment);
+        $this->assertEquals(null, $testOne->secondInstallment);
+        $this->assertEquals(null, $testOne->thirdInstallment);
+        $this->assertEquals(null, $testOne->forthInstallment);
+        $this->assertEquals(null, $testOne->fifthInstallment);
+        $this->assertEquals(100.5, $testOne->sixthInstallment);
+    }
+
+    public function testFactoryInvoiceWithNewYearsEveOnSecondInstallment()
+    {
+        $dtoOne = new InvoiceItemDTO(1, 50, 'Test-one', 100.5, '2024-1', 6);
+        $testOne = InvoiceFactory::factoryInvoice($dtoOne, 12);
+
+        $this->assertEquals(1, $testOne->id);
+        $this->assertEquals(50, $testOne->countId);
+        $this->assertEquals('Test-one', $testOne->name);
+        $this->assertEquals(603, $testOne->totalRemainingValue);
+        $this->assertEquals(null, $testOne->firstInstallment);
+        $this->assertEquals(100.5, $testOne->secondInstallment);
+        $this->assertEquals(100.5, $testOne->thirdInstallment);
+        $this->assertEquals(100.5, $testOne->forthInstallment);
+        $this->assertEquals(100.5, $testOne->fifthInstallment);
+        $this->assertEquals(100.5, $testOne->sixthInstallment);
+    }
+
+    public function testFactoryInvoiceWithNewYearsEveOnThirdInstallment()
+    {
+        $dtoOne = new InvoiceItemDTO(1, 50, 'Test-one', 100.5, '2024-1', 6);
+        $testOne = InvoiceFactory::factoryInvoice($dtoOne, 11);
+
+        $this->assertEquals(1, $testOne->id);
+        $this->assertEquals(50, $testOne->countId);
+        $this->assertEquals('Test-one', $testOne->name);
+        $this->assertEquals(603, $testOne->totalRemainingValue);
+        $this->assertEquals(null, $testOne->firstInstallment);
+        $this->assertEquals(null, $testOne->secondInstallment);
+        $this->assertEquals(100.5, $testOne->thirdInstallment);
+        $this->assertEquals(100.5, $testOne->forthInstallment);
+        $this->assertEquals(100.5, $testOne->fifthInstallment);
+        $this->assertEquals(100.5, $testOne->sixthInstallment);
+    }
+
+    public function testFactoryInvoiceWithNewYearsEveOnFourthInstallment()
+    {
+        $dtoOne = new InvoiceItemDTO(1, 50, 'Test-one', 100.5, '2024-1', 6);
+        $testOne = InvoiceFactory::factoryInvoice($dtoOne, 10);
+
+        $this->assertEquals(1, $testOne->id);
+        $this->assertEquals(50, $testOne->countId);
+        $this->assertEquals('Test-one', $testOne->name);
+        $this->assertEquals(603, $testOne->totalRemainingValue);
+        $this->assertEquals(null, $testOne->firstInstallment);
+        $this->assertEquals(null, $testOne->secondInstallment);
+        $this->assertEquals(null, $testOne->thirdInstallment);
+        $this->assertEquals(100.5, $testOne->forthInstallment);
+        $this->assertEquals(100.5, $testOne->fifthInstallment);
+        $this->assertEquals(100.5, $testOne->sixthInstallment);
+    }
+
+    public function testFactoryInvoiceWithNewYearsEveOnFifthInstallment()
+    {
+        $dtoOne = new InvoiceItemDTO(1, 50, 'Test-one', 100.5, '2024-2', 6);
+        $testOne = InvoiceFactory::factoryInvoice($dtoOne, 10);
+
+        $this->assertEquals(1, $testOne->id);
+        $this->assertEquals(50, $testOne->countId);
+        $this->assertEquals('Test-one', $testOne->name);
+        $this->assertEquals(603, $testOne->totalRemainingValue);
+        $this->assertEquals(null, $testOne->firstInstallment);
+        $this->assertEquals(null, $testOne->secondInstallment);
+        $this->assertEquals(null, $testOne->thirdInstallment);
+        $this->assertEquals(null, $testOne->forthInstallment);
+        $this->assertEquals(100.5, $testOne->fifthInstallment);
+        $this->assertEquals(100.5, $testOne->sixthInstallment);
+    }
+
+    public function testFactoryInvoiceWithNewYearsEveOnSixthInstallment()
+    {
+        $dtoOne = new InvoiceItemDTO(1, 50, 'Test-one', 100.5, '2024-3', 6);
+        $testOne = InvoiceFactory::factoryInvoice($dtoOne, 10);
+
+        $this->assertEquals(1, $testOne->id);
+        $this->assertEquals(50, $testOne->countId);
+        $this->assertEquals('Test-one', $testOne->name);
+        $this->assertEquals(603, $testOne->totalRemainingValue);
+        $this->assertEquals(null, $testOne->firstInstallment);
+        $this->assertEquals(null, $testOne->secondInstallment);
+        $this->assertEquals(null, $testOne->thirdInstallment);
+        $this->assertEquals(null, $testOne->forthInstallment);
+        $this->assertEquals(null, $testOne->fifthInstallment);
+        $this->assertEquals(100.5, $testOne->sixthInstallment);
+    }
 }
