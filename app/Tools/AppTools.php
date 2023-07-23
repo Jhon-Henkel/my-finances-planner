5ca166fb2ca06222e2bca791db0bb0ea2dd53f74
Jhonatan Henkel <jhowrf@gmail.com>
2023-07-23 10:46:45 -0300

    Centralizando busca do env, alterado telas restantes para ter o loading real

--- app/Tools/AppTools.php
+++ app/Tools/AppTools.php
@@ -4,7 +4,7 @@ namespace App\Tools;
 
 use App\Http\Kernel;
 
-class ApplicationTools
+class AppTools
 {
     /**
      * @codeCoverageIgnore
@@ -14,5 +14,13 @@ class ApplicationTools
     {
         $app = require_once __DIR__ . '/../../bootstrap/app.php';
         $app->make(Kernel::class)->bootstrap();
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public static function getEnvValue(string $key): string
+    {
+        return env($key);
     }
 }
