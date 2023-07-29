bfecd564247bd8ef03265a23aa9dd64e7f90f1cd
Jhonatan Henkel <jhowrf@gmail.com>
2023-07-29 16:19:09 -0300

    Desenvolvendo factory e seeder para o model futureGain

--- app/Models/FutureGain.php
+++ app/Models/FutureGain.php
@@ -4,11 +4,12 @@ namespace App\Models;
 
 use App\Enums\DateEnum;
 use App\Models\Trait\Tenantable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class FutureGain extends Model
 {
-    use Tenantable;
+    use Tenantable, HasFactory;
 
     protected $table = 'future_gain';
     protected $fillable = ['id', 'wallet_id', 'description', 'amount', 'forecast', 'installments'];
