59b763c6650d9bf9ed6267a36e6ece101665fd61
Jhonatan Henkel <jhowrf@gmail.com>
2023-07-29 17:31:59 -0300

    Desenvolvendo factory e seeder para o model MonthlyClosing

--- app/Models/MonthlyClosing.php
+++ app/Models/MonthlyClosing.php
@@ -4,11 +4,12 @@ namespace App\Models;
 
 use App\Enums\DateEnum;
 use App\Models\Trait\Tenantable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class MonthlyClosing extends Model
 {
-    use Tenantable;
+    use Tenantable, HasFactory;
 
     protected $table = 'monthly_closing';
     protected $fillable = ['id', 'predicted_earnings', 'predicted_expenses', 'real_earnings', 'real_expenses', 'balance'];
--- app/Tools/DemoApplicationTools.php
+++ app/Tools/DemoApplicationTools.php
@@ -2,13 +2,7 @@
 
 namespace App\Tools;
 
-use App\Models\MonthlyClosing;
-use Database\Seeders\CreditCardSeeder;
-use Database\Seeders\CreditCardTransactionSeeder;
-use Database\Seeders\FutureGainSeeder;
-use Database\Seeders\FutureSpentSeeder;
-use Database\Seeders\MovementsSeeder;
-use Database\Seeders\WalletSeeder;
+use Database\Seeders\DatabaseSeeder;
 use Illuminate\Support\Facades\DB;
 
 /**
@@ -38,60 +32,7 @@ class DemoApplicationTools
         if (! RequestTools::isApplicationInDemoMode()) {
             return false;
         }
-        (new WalletSeeder)->run();
-        (new CreditCardSeeder)->run();
-        (new CreditCardTransactionSeeder)->run();
-        (new FutureGainSeeder)->run();
-        (new FutureSpentSeeder)->run();
-        (new MovementsSeeder)->run();
-        self::insertMonthlyClosing();
+        (new DatabaseSeeder())->run();
         return true;
-    }
-
-    protected static function insertMonthlyClosing(): void
-    {
-        $monthlyClosingModel = app(MonthlyClosing::class);
-        $data = self::makeDemoMonthlyClosing();
-        foreach ($data as $item) {
-            $monthlyClosingModel->create($item)->toArray();
-        }
-    }
-
-    protected static function makeDemoMonthlyClosing(): array
-    {
-        return [
-            [
-                'id' => 1,
-                'predicted_earnings' => 1230,
-                'predicted_expenses' => 1000,
-                'real_earnings' => 2000,
-                'real_expenses' => 1500,
-                'balance' => 500,
-            ],
-            [
-                'id' => 2,
-                'predicted_earnings' => 1200,
-                'predicted_expenses' => 1300,
-                'real_earnings' => 1450,
-                'real_expenses' => 1500,
-                'balance' => -50,
-            ],
-            [
-                'id' => 3,
-                'predicted_earnings' => 1500,
-                'predicted_expenses' => 1450,
-                'real_earnings' => 1561,
-                'real_expenses' => 1000,
-                'balance' => 561
-            ],
-            [
-                'id' => 4,
-                'predicted_earnings' => 1350,
-                'predicted_expenses' => 1450,
-                'real_earnings' => 1390,
-                'real_expenses' => 1490,
-                'balance' => -100,
-            ]
-        ];
     }
 }
