394eb86596cd11b6d4894b0130abbab593b0d66b
Jhonatan Henkel <jhowrf@gmail.com>
2023-08-03 19:51:33 -0300

    Removendo o unique
    Removendo o unique no name das tabelas de configurações e carteiras.

---
+++ database/migrations/2023_08_03_194419_updating_unique_fields.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('wallets', function (Blueprint $table) {
+            $table->dropUnique('wallets_name_unique');
+        });
+
+        Schema::table('configurations', function (Blueprint $table) {
+            $table->dropUnique('configurations_name_unique');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('wallets', function (Blueprint $table) {
+            $table->unique('name');
+        });
+
+        Schema::table('configurations', function (Blueprint $table) {
+            $table->unique('name');
+        });
+    }
+};
