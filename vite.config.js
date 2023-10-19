d478c30014a7c690cf65d56c87e006b4e2c275b6
Jhonatan Henkel <96666648+Jhon-Henkel@users.noreply.github.com>
2023-10-19 08:00:28 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 import { fileURLToPath, URL } from 'node:url'
-import { resolve } from 'path'
+import { resolve } from 'node:path'
 
 export default defineConfig({
     server: {
