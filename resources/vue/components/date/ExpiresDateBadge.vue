1dcc27148665f5f1df43f49c9e97f585ad887849
Jhonatan Henkel <jhowrf@gmail.com>
2023-11-11 11:35:44 -0300

    Criado componente para badge de dia do vencimento

---
+++ resources/vue/components/date/ExpiresDateBadge.vue
@@ -0,0 +1,43 @@
+<template>
+    <span class="badge rounded-2"
+          :class="getBadgeTypeForForecastDate(installment)"
+          v-tooltip="getTitleForForecastDate(installment)">
+        {{ installment.nextInstallmentDay }}
+    </span>
+</template>
+
+<script>
+import CalendarTools from '~js/tools/calendarTools'
+
+export default {
+    name: 'MfpExpiresDateBadge',
+    props: {
+        installment: {
+            type: Object,
+            required: true
+        }
+    },
+    methods: {
+        getBadgeTypeForForecastDate(installment) {
+            const today = CalendarTools.getToday().getDate()
+            const nextInstallmentDay = parseInt(installment.nextInstallmentDay)
+            if ((nextInstallmentDay < today) && (installment.firstInstallment > 0)) {
+                return 'bg-danger'
+            } else if ((nextInstallmentDay > today) && (installment.firstInstallment > 0)) {
+                return 'bg-warning'
+            }
+            return 'bg-success'
+        },
+        getTitleForForecastDate(installment) {
+            const today = CalendarTools.getToday().getDate()
+            const nextInstallmentDay = parseInt(installment.nextInstallmentDay)
+            if ((nextInstallmentDay < today) && (installment.firstInstallment > 0)) {
+                return 'Atrasado'
+            } else if ((nextInstallmentDay > today) && (installment.firstInstallment > 0)) {
+                return 'Prestes a Vencer'
+            }
+            return 'Em dia'
+        }
+    }
+}
+</script>
---
+++ spec/resourses/vue/components/date/ExpiresDateBadge.test.js
@@ -0,0 +1,66 @@
+import { describe, it, expect, vitest } from 'vitest'
+import { shallowMount } from '@vue/test-utils'
+import MfpExpiresDateBadge from '~vue-component/date/ExpiresDateBadge.vue'
+import calendarTools from '~js/tools/calendarTools'
+
+describe("Testing ExpiresDateBadge component", () => {
+    it ("Render DatepickerRange Success", async () => {
+        vitest.spyOn(calendarTools, 'getToday').mockImplementation(() => {
+            return new Date('2023-10-16')
+        })
+        const wrapper = shallowMount(MfpExpiresDateBadge, {
+            propsData: {
+                installment: {
+                    nextInstallmentDay: '16',
+                    firstInstallment: 0
+                }
+            },
+            global: {
+                directives: {
+                    tooltip: true
+                }
+            }
+        })
+        expect(wrapper.html()).toContain('<span class="badge rounded-2 bg-success">16</span>')
+    })
+
+    it ("Render DatepickerRange Danger", async () => {
+        vitest.spyOn(calendarTools, 'getToday').mockImplementation(() => {
+            return new Date('2023-10-16')
+        })
+        const wrapper = shallowMount(MfpExpiresDateBadge, {
+            propsData: {
+                installment: {
+                    nextInstallmentDay: '2',
+                    firstInstallment: 1
+                }
+            },
+            global: {
+                directives: {
+                    tooltip: true
+                }
+            }
+        })
+        expect(wrapper.html()).toContain('<span class="badge rounded-2 bg-danger">2</span>')
+    })
+
+    it ("Render DatepickerRange Warning", async () => {
+        vitest.spyOn(calendarTools, 'getToday').mockImplementation(() => {
+            return new Date('2023-10-16')
+        })
+        const wrapper = shallowMount(MfpExpiresDateBadge, {
+            propsData: {
+                installment: {
+                    nextInstallmentDay: '20',
+                    firstInstallment: 1
+                }
+            },
+            global: {
+                directives: {
+                    tooltip: true
+                }
+            }
+        })
+        expect(wrapper.html()).toContain('<span class="badge rounded-2 bg-warning">20</span>')
+    })
+})
