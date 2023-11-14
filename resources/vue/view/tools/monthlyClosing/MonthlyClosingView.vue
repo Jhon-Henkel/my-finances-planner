3dd48caeb50b36e7718909c7817a89e0ed7a5e4a
Jhonatan Henkel <96666648+Jhon-Henkel@users.noreply.github.com>
2023-11-14 06:49:57 -0300

    Ajuste na busca do fechamento de mês (#398)
    * Ajuste na busca do fechamento de mês para ser o periodo do ano atual na busca inicial

--- resources/vue/view/tools/monthlyClosing/MonthlyClosingView.vue
+++ resources/vue/view/tools/monthlyClosing/MonthlyClosingView.vue
@@ -1,4 +1,4 @@
-<template>
+(<template>
     <div class="base-container">
         <loading-component v-show="loadingDone === false" />
         <div v-show="loadingDone">
@@ -134,7 +134,9 @@ export default {
         }
     },
     async mounted() {
-        await this.getMonthlyClosingsIndexFiltered()
+        const thisYearPeriod = CalendarTools.getThisYearPeriod()
+        const quest = `?dateStart=${thisYearPeriod[0]}&dateEnd=${thisYearPeriod[0]}`
+        await this.getMonthlyClosingsIndexFiltered(quest)
     }
 }
 </script>
