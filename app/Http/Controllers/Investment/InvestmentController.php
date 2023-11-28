bb0676b4290c34764e2b378ce896ded3aa662367
Jhonatan Henkel <jhowrf@gmail.com>
2023-11-28 20:33:55 -0300

    Desenvolvido controller

--- app/Http/Controllers/Investment/InvestmentController.php
+++ app/Http/Controllers/Investment/InvestmentController.php
@@ -3,26 +3,48 @@
 namespace App\Http\Controllers\Investment;
 
 use App\Http\Controllers\BasicController;
+use App\Resources\Investment\InvestmentResource;
+use App\Services\Investment\InvestmentService;
 
 class InvestmentController extends BasicController
 {
-    public function __construct()
-    {
+    public function __construct(
+        readonly private InvestmentService $service,
+        readonly private InvestmentResource $resource
+    ) {
     }
 
     protected function rulesInsert(): array
     {
+        return [
+            'credit_card_id' => 'int|exists:App\Models\CreditCard,id',
+            'description' => 'required|max:255|string',
+            'type' => 'required|numeric',
+            'amount' => 'required|decimal:0,2',
+            'liquidity' => 'required|numeric',
+            'profitability' => 'required|decimal:0,2'
+        ];
     }
 
     protected function rulesUpdate(): array
     {
+        return [
+            'credit_card_id' => 'int|exists:App\Models\CreditCard,id',
+            'description' => 'required|max:255|string',
+            'type' => 'required|numeric',
+            'amount' => 'required|decimal:0,2',
+            'liquidity' => 'required|numeric',
+            'profitability' => 'required|decimal:0,2'
+        ];
     }
 
-    protected function getService()
+    protected function getService(): InvestmentService
     {
+        return $this->service;
     }
 
-    protected function getResource()
+    protected function getResource(): InvestmentResource
     {
+        return $this->resource;
     }
 }
