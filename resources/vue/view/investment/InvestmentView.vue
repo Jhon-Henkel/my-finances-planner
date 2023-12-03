a3df63485705b8e125a9d313ba8ab55ac2775b40
Jhonatan Henkel <jhowrf@gmail.com>
2023-12-03 14:58:24 -0300

    Implementando dashboard de investimentos

--- resources/vue/view/investment/InvestmentView.vue
+++ resources/vue/view/investment/InvestmentView.vue
@@ -15,14 +15,42 @@
                 <div class="col-4">
                     <div class="card glass">
                         <div class="card-body text-center">
-                            <bar-chart :graph-options="graphOptions" :chart-data="chartData" />
+                            <bar-chart :graph-options="investmentGraphOptions" :chart-data="investmentTypesChartData" />
                         </div>
                     </div>
                 </div>
-                <div class="col-8">
+                <div class="col-4 mobile-class">
+                    <div class="card glass full-height">
+                        <div class="card-body text-center">
+                            <h4 class="card-title">
+                                <font-awesome-icon :icon="iconEnum.piggyBank()" class="me-2"/>
+                                Total Investido
+                            </h4>
+                            <hr>
+                            <p class="card-text">{{ totalInvested }}</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4 mobile-class">
+                    <div class="card glass full-height">
+                        <div class="card-body text-center">
+                            <h4 class="card-title">
+                                <font-awesome-icon :icon="iconEnum.billTrendUp()" class="me-2"/>
+                                Principais indices hoje
+                            </h4>
+                            <hr>
+                            <p class="card-text">Selic {{ taxToday.selic }} % ao ano</p>
+                            <p class="card-text">IPCA {{ taxToday.ipca }} % ao ano</p>
+                            <p class="card-text">CDI {{ taxToday.cdi }} % ao ano</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row ms-2 chart mt-2">
+                <div class="col-7 mobile-class">
                     <div class="card glass">
                         <div class="card-body text-center">
-
+                            <bar-chart :graph-options="investmentGraphOptions" :chart-data="investmentMovementsChartData" />
                         </div>
                     </div>
                 </div>
@@ -44,10 +72,15 @@ import apiRouter from '~js/router/apiRouter'
 import messageTools from '~js/tools/messageTools'
 import BarChart from '~vue-component/graphics/BarChart.vue'
 import investmentChartParams from '~js/chartParams/investmentChartParams'
+import StringTools from '~js/tools/stringTools'
+import InvestmentTools from '~js/tools/investmentTools'
 
 export default {
     name: 'InvestmentView',
     computed: {
+        StringTools() {
+            return StringTools
+        },
         iconEnum() {
             return IconEnum
         },
@@ -68,8 +101,17 @@ export default {
             messageData: {},
             loadingDone: true,
             investments: [],
-            graphOptions: investmentChartParams.options,
-            chartData: investmentChartParams.data,
+            contributedAndRescued: [
+                {
+                    labels: [],
+                    contributed: [],
+                    rescued: []
+                }
+            ],
+            investmentGraphOptions: investmentChartParams.options,
+            investmentMovementsGraphOptions: investmentChartParams.options,
+            investmentTypesChartData: investmentChartParams.data,
+            investmentMovementsChartData: investmentChartParams.data,
             buttonsNewData: [
                 {
                     title: 'Novo CDB',
@@ -83,22 +125,33 @@ export default {
                     icon: IconEnum.billTrendUp(),
                     redirectTo: '/investimentos/cdb'
                 }
-            ]
+            ],
+            response: {
+                contributedAndRescued: []
+            },
+            totalInvested: 0,
+            taxToday: {
+                cdi: 0,
+                selic: 0,
+                ipca: 0
+            }
         }
     },
     methods: {
         async getInvestmentsDataGraph() {
             await apiRouter.investments.dataGraph().then(response => {
-                console.log(response)
                 this.investments = response
+                this.contributedAndRescued = response.contributedAndRescued
                 this.populateDataGraphInvestmentsPerType()
+                this.populateDataGraphContributedAndRescued()
+                this.totalInvested = StringTools.formatFloatValueToBrString(response.cdb.value)
             }).catch(error => {
                 this.messageData = messageTools.errorMessage(error.response.data.message)
             })
             this.loadingDone = true
         },
         populateDataGraphInvestmentsPerType() {
-            this.chartData = {
+            this.investmentTypesChartData = {
                 labels: [this.investments.cdb.label],
                 datasets: [
                     {
@@ -108,10 +161,34 @@ export default {
                     }
                 ]
             }
+        },
+        populateDataGraphContributedAndRescued() {
+            this.investmentMovementsGraphOptions.plugins.title.text = 'Aportes e Resgates'
+            this.investmentMovementsChartData = {
+                labels: this.contributedAndRescued.labels,
+                datasets: [
+                    {
+                        label: 'Contribuições',
+                        backgroundColor: '#1ead98',
+                        data: this.contributedAndRescued.contributed
+                    },
+                    {
+                        label: 'Resgates',
+                        backgroundColor: '#ff0000',
+                        data: this.contributedAndRescued.rescued
+                    }
+                ]
+            }
+        },
+        async getTaxToday() {
+            await InvestmentTools.getBrasilTax().then(response => {
+                this.taxToday = response
+            })
         }
     },
     mounted() {
         this.investments = this.getInvestmentsDataGraph()
+        this.getTaxToday()
     }
 }
 </script>
@@ -121,9 +198,16 @@ export default {
         display: inline-flex;
         width: 100%;
     }
+    .full-height {
+        height: 100%;
+    }
     @media (max-width: 1000px) {
         .me-2 {
             margin-right: 0 !important;
+        }
+        .mobile-class {
+            width: 100%;
+            margin-top: 25px;
         }
     }
 </style>
