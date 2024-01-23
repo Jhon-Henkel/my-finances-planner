b4eec19ba03b7b677de47020ab76cf1fec5372ac
Jhonatan Henkel <96666648+Jhon-Henkel@users.noreply.github.com>
2024-01-23 12:58:08 -0300

    fix: make truncate of new tables in demo mode

--- app/Tools/DemoApplicationTools.php
+++ app/Tools/DemoApplicationTools.php
@@ -20,10 +20,12 @@ class DemoApplicationTools
         DB::table('movements')->truncate();
         DB::table('future_gain')->truncate();
         DB::table('future_spent')->truncate();
+        DB::table('credit_card_movement')->truncate();
         DB::table('credit_card_transaction')->truncate();
         DB::table('credit_card')->truncate();
         DB::table('wallets')->truncate();
         DB::table('monthly_closing')->truncate();
+        DB::table('investment')->truncate();
         DB::statement('SET FOREIGN_KEY_CHECKS=1;');
         return true;
     }
