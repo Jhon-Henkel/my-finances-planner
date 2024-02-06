b297bab77a7b592d40808c692f709b8ae9b690cc
Jhonatan Henkel <96666648+Jhon-Henkel@users.noreply.github.com>
2024-02-06 20:49:51 -0300

    feat: update UI from manageCardsView (#504)
    * feat: update UI from manageCardsView
    
    * fix: fix broken tests

--- tests/frontend/resourses/vue/view/creditCard/ManageCardsView.test.js
+++ tests/frontend/resourses/vue/view/creditCard/ManageCardsView.test.js
@@ -24,6 +24,16 @@ describe('Testing the manage cards basic render', () => {
                 },
             }
         })
-        expect(wrapper.html()).toContain('class="col-12 text-center mt-4 mb-4"> Você não possui nenhum cartão cadastrado! </div>')
+        expect(wrapper.html()).toContain('<mfp-message-stub ')
+        expect(wrapper.html()).toContain('<loading-component-stub ')
+        expect(wrapper.html()).toContain('class="nav mt-2 justify-content-end">')
+        expect(wrapper.html()).toContain('title="Cartões"></mfp-title-stub>')
+        expect(wrapper.html()).toContain('<mfp-drop-down-button-stub ')
+        expect(wrapper.html()).toContain('dropdowntitle="Novo" dropdownicon="fas,circle-plus" alignitenscenter="true"></mfp-drop-down-button-stub>')
+        expect(wrapper.html()).toContain('<divider-stub ')
+        expect(wrapper.html()).toContain('class="glass card mb-2">')
+        expect(wrapper.html()).toContain('class="col-12 text-center"> Você não possui nenhum cartão cadastrado! </div>')
+        expect(wrapper.html()).toContain('class="form-select card-select-to-pay" id="pay-invoice" disabled="" style="display: none;">')
+        expect(wrapper.html()).toContain('<option data-v-5bf0dfc0="" disabled="" value="0">Selecione a carteira</option>')
     })
 })
