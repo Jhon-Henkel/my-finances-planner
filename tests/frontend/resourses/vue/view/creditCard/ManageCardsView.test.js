b47a55cdbeb554e82e1b3afc4762c15e29411531
Jhonatan Henkel <jhowrf@gmail.com>
2024-02-07 21:12:33 -0300

    feat: styling hr on manageCardsView screen

--- tests/frontend/resourses/vue/view/creditCard/ManageCardsView.test.js
+++ tests/frontend/resourses/vue/view/creditCard/ManageCardsView.test.js
@@ -1,9 +1,9 @@
 import { describe, it, expect, vitest } from 'vitest'
 import {RouterLinkStub, shallowMount} from '@vue/test-utils'
-import ApiRouter from "~js/router/apiRouter";
-import ManageCardsView from "~vue/view/creditCard/ManageCardsView.vue";
+import ApiRouter from '~js/router/apiRouter'
+import ManageCardsView from '~vue/view/creditCard/ManageCardsView.vue'
 
-describe('Testing the manage cards basic render', () => {
+describe('Testing the manage cards render', () => {
     it('should render manage cards basic screen view without cards registered', async () => {
         vitest.spyOn(ApiRouter.cards, 'index').mockImplementation(async () => {
             return []
