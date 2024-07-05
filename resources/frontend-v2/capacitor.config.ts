b94b202fc625e8ff027bed97d138170dadffa5b0
jhowrf <jhowrf@gmail.com>
2024-07-05 20:55:34 -0300

    feat: apply code-stile on existent files

--- resources/frontend-v2/capacitor.config.ts
+++ resources/frontend-v2/capacitor.config.ts
@@ -1,9 +1,9 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type {CapacitorConfig} from '@capacitor/cli'
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'mfp-frontend-v2',
-  webDir: 'dist'
-};
+    appId: 'io.ionic.starter',
+    appName: 'mfp-frontend-v2',
+    webDir: 'dist'
+}
 
-export default config;
+export default config
