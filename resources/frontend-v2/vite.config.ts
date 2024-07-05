6cc57c68ee44b58212e063649af7c60066dbb7b2
jhowrf <jhowrf@gmail.com>
2024-07-05 20:47:11 -0300

    feat: configure build for frontend-v2

--- resources/frontend-v2/vite.config.ts
+++ resources/frontend-v2/vite.config.ts
@@ -1,23 +1,33 @@
 /// <reference types="vitest" />
-
 import legacy from '@vitejs/plugin-legacy'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
-import { defineConfig } from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    legacy()
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom'
-  }
+export default defineConfig((): any => {
+    const env = loadEnv('', process.cwd())
+    return {
+        plugins: [
+            vue(),
+            legacy()
+        ],
+        define: {
+            'process.env': env
+        },
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, './src'),
+            },
+        },
+        build: {
+            manifest: true,
+            outDir: '../../public/build-ionic',
+            emptyOutDir: true,
+        },
+        base: env.VITE_ENV === 'production' ? '/public/build-ionic/' : '/',
+        test: {
+            globals: true,
+            environment: 'jsdom'
+        }
+    }
 })
