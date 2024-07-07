e97f367ad0cb5de6d33dae8f9e0422dcb46b2292
jhowrf <jhowrf@gmail.com>
2024-07-07 18:01:18 -0300

    feat: create base screen for wallets and apply auth middleware on router in frontend v2

--- resources/frontend-v2/src/router/index.ts
+++ resources/frontend-v2/src/router/index.ts
@@ -1,8 +1,13 @@
 import {createRouter, createWebHistory} from '@ionic/vue-router'
 import {RouteRecordRaw} from 'vue-router'
 import TabsPage from '../views/TabsPage.vue'
+import RouterAuthMiddleware from "@/router/RouterAuthMiddleware"
 
 const routes: Array<RouteRecordRaw> = [
+    {
+        path: '/v2',
+        redirect: '/v2/login'
+    },
     {
         path: '/v2/login',
         name: 'login',
@@ -12,6 +17,20 @@ const routes: Array<RouteRecordRaw> = [
         path: '/v2/em-breve',
         name: 'in-development',
         component: () => import('@/views/in-development/InDevelopmentPage.vue')
+    },
+    {
+        path: '/v2/',
+        component: TabsPage,
+        children: [
+            {
+                path: 'carteiras',
+                name: 'wallets',
+                component: () => import('@/views/wallets/WalletsPage.vue')
+            },
+        ],
+        meta: {
+            auth: true
+        }
     },
     {
         path: '/v2/tabs/',
@@ -41,5 +60,7 @@ const router = createRouter({
     history: createWebHistory(),
     routes
 })
+
+router.beforeEach(RouterAuthMiddleware)
 
 export default router
