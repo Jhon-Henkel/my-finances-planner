8a96424a7f7b5e0cfef5fed02eeb880b2b5fdb86
jhowrf <jhowrf@gmail.com>
2024-07-08 21:21:31 -0300

    feat: create modal component

---
+++ resources/frontend-v2/src/components/modal/MfpModalHeader.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import {IonButton, IonButtons, IonHeader, IonTitle, IonToolbar} from "@ionic/vue"
+
+defineProps(
+    {
+        title: {
+            type: String,
+            required: true
+        },
+        saveActionLabel: {
+            type: String,
+            default: 'Salvar',
+            required: false
+        },
+        saveActionButtonDisabled: {
+            type: Boolean,
+            default: false,
+            required: false
+        }
+    }
+)
+
+const emits = defineEmits(['close-action', 'save-action'])
+</script>
+
+<template>
+    <ion-header>
+        <ion-toolbar>
+            <ion-title>{{ title }}</ion-title>
+            <ion-buttons slot="start">
+                <ion-button @click="emits('close-action')">
+                    Fechar
+                </ion-button>
+            </ion-buttons>
+            <ion-buttons slot="end">
+                <ion-button @click="emits('save-action')" :disabled="saveActionButtonDisabled">
+                    {{ saveActionLabel }}
+                </ion-button>
+            </ion-buttons>
+        </ion-toolbar>
+    </ion-header>
+</template>
