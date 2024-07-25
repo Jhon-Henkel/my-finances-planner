4bcd707e6095880e794ab9826bed551ecdbc77f2
jhowrf <jhowrf@gmail.com>
2024-07-25 13:07:45 -0300

    feat: adjust card of panorama

--- resources/frontend-v2/src/views/panorama/MfpPanoramaDetailsCardItem.vue
+++ resources/frontend-v2/src/views/panorama/MfpPanoramaDetailsCardItem.vue
@@ -28,24 +28,35 @@ defineProps({
 
 <template>
     <ion-row>
-        <ion-col size="12" class="ion-text-end">
-            <ion-label class="center-ion-label-content">
-                <ion-icon :icon="icon" :color="iconColor" class="icon"/>
-                <ion-text class="ion-margin-end">
-                    {{ title }}
-                </ion-text>
-                <ion-badge v-if="useBadge" :color="value > 0 ? 'success' : 'danger'">
-                    <mfp-counter-money :end="value" v-if="typeof value === 'number'"/>
-                    <ion-text v-else>
-                        {{ value }}
-                    </ion-text>
-                </ion-badge>
-                <ion-text v-else>
-                    <mfp-counter-money :end="value" v-if="typeof value === 'number'"/>
-                    <ion-text v-else>
-                        {{ value }}
-                    </ion-text>
-                </ion-text>
+        <ion-col size="12" class="ion-text-end ion-no-padding">
+            <ion-label class="ion-text-start">
+                <ion-row class="vertical-center">
+                    <ion-col size="2" class="ion-no-padding">
+                        <ion-icon :icon="icon" :color="iconColor" class="icon"/>
+                    </ion-col>
+                    <ion-col size="5" class="ion-no-padding">
+                        <ion-text>
+                            {{ title }}
+                        </ion-text>
+                    </ion-col>
+                    <ion-col size="5" class="ion-no-padding">
+                        <ion-badge
+                            v-if="useBadge"
+                            :color="typeof value === 'number' && value > 0 ? 'success' : 'danger'"
+                        >
+                            <mfp-counter-money :end="value" v-if="typeof value === 'number'"/>
+                            <ion-text v-else>
+                                {{ value }}
+                            </ion-text>
+                        </ion-badge>
+                        <ion-text v-else>
+                            <mfp-counter-money :end="value" v-if="typeof value === 'number'"/>
+                            <ion-text v-else>
+                                {{ value }}
+                            </ion-text>
+                        </ion-text>
+                    </ion-col>
+                </ion-row>
             </ion-label>
         </ion-col>
     </ion-row>
@@ -54,6 +65,9 @@ defineProps({
 <style scoped>
 .icon {
     font-size: 1.5em;
-    margin-right: 10px;
+}
+.vertical-center {
+    display: flex;
+    align-items: center;
 }
 </style>
