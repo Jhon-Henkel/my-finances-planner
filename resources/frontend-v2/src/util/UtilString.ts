bf7cc95e06948aa70686a6b6751f65dda3286ef1
Jhonatan Henkel <96666648+Jhon-Henkel@users.noreply.github.com>
2024-07-25 12:47:50 -0300

    fix: auto capitalize first letters from words on mfp input in case of input type is text (#662)

--- resources/frontend-v2/src/util/UtilString.ts
+++ resources/frontend-v2/src/util/UtilString.ts
@@ -6,7 +6,12 @@ export const UtilString = {
         }
         return string
     },
-    capitalizeFirstLetter: function(string: string): string {
+    capitalizeFirstLetter: function (string: string): string {
         return string.charAt(0).toUpperCase() + string.slice(1)
+    },
+    capitalizeFirstLetters: function (string: string): string {
+        return string.split(' ').map(word => {
+            return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+        }).join(' ')
     }
 }
