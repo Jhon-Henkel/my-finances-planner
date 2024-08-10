ae2a5a7324997cde894cfa1e6216b56179522282
jhowrf <jhowrf@gmail.com>
2024-08-10 10:57:44 -0300

    Add Codecov bundler plugin

--- resources/frontend-v2/src/vite-env.d.ts
+++ resources/frontend-v2/src/vite-env.d.ts
@@ -5,6 +5,7 @@ interface ImportMetaEnv {
     readonly VITE_API_BASE_URL: string;
     readonly VITE_MFP_TOKEN: string;
     readonly VITE_SENTRY_DSN_PUBLIC: string;
+    readonly VITE_CODECOV_TOKEN: string;
 }
 
 interface ImportMeta {
