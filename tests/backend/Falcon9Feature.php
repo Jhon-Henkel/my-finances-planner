43c7ccc3f0473bee3818facc97572621bc23c9f5
jhowrf <jhowrf@gmail.com>
2024-09-02 06:54:27 -0300

    feat: validate user block by wrong login attempts

--- tests/backend/Falcon9Feature.php
+++ tests/backend/Falcon9Feature.php
@@ -4,6 +4,8 @@ namespace Tests\backend;
 
 use App\Enums\StatusEnum;
 use App\Models\User;
+use App\Services\Mail\MailService;
+use App\Services\Queue\QueueProducerService;
 use App\Tools\Auth\JwtTools;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -22,6 +24,8 @@ abstract class Falcon9Feature extends BaseTestCase
         parent::setUp();
         session()->flush();
         unset($_SERVER['HTTP_X_MFP_USER_TOKEN']);
+        $_SERVER['REMOTE_ADDR'] = '127.0.0.1';
+        $_SERVER['HTTP_USER_AGENT'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3';
         $query = "SELECT * FROM users WHERE email = 'demo@demo.dev'";
         $user = DB::select($query);
         if (empty($user)) {
@@ -46,7 +50,7 @@ abstract class Falcon9Feature extends BaseTestCase
         return [
             'Content-Type' => 'application/json',
             'Accept' => 'application/json',
-            'MFP-TOKEN' => env('PUSHER_APP_KEY'),
+            'MFP-TOKEN' => config('app.mfp_token'),
             'X-MFP-USER-TOKEN' => 'Bearer ' . JwtTools::createJWT($this->user)
         ];
     }
@@ -57,5 +61,43 @@ abstract class Falcon9Feature extends BaseTestCase
         $this->artisan('config:clear');
         $this->artisan('route:clear');
         $this->artisan('view:clear');
+    }
+
+    protected function createNewUser(): array
+    {
+        $userData = [
+            'name' => $this->faker->name,
+            'email' => $this->faker->email,
+            'password' => $this->faker->password,
+            'confirmPassword' => $this->faker->password,
+        ];
+
+        $queueData = null;
+        $queueMock = $this->mock(QueueProducerService::class);
+        $queueMock->shouldReceive('produce')->andReturnUsing(
+            function ($data) use (&$queueData) {
+                $data->addAdditionDate();
+                $queueData = $data->toJson();
+            }
+        );
+
+        $this->postJson('/user/register', $userData);
+        $data = json_decode($queueData, true);
+
+        $this->postJson('/api/mfp/user/register/step-one', [$data['data']], $this->makeHeaders());
+        $data = json_decode($queueData, true);
+
+        $mailData = null;
+        $mockMail = $this->mock(MailService::class);
+        $mockMail->shouldReceive('sendEmail')->andReturnUsing(
+            function ($data) use (&$mailData) {
+                $mailData = $data;
+            }
+        );
+
+        $this->postJson('/api/mfp/user/register/step-two', [$data['data']], $this->makeHeaders());
+        $this->postJson("/api/mfp/user/register/activate/{$mailData->getParams()['hash']}", [], $this->makeHeaders());
+
+        return $userData;
     }
 }
---
+++ tests/backend/Feature/UseCases/User/WrongUserLoginUseCaseTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Tests\backend\Feature\UseCases\User;
+
+use App\Enums\StatusEnum;
+use App\Models\User;
+use PHPUnit\Framework\Attributes\Test;
+use Tests\backend\Falcon9Feature;
+
+class WrongUserLoginUseCaseTest extends Falcon9Feature
+{
+    private array $headers;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->headers = [
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/json',
+            'MFP-TOKEN' => config('app.mfp_token'),
+        ];
+    }
+
+    #[Test]
+    public function blockUser()
+    {
+        // Testando login com sucesso
+        $user = $this->createNewUser();
+
+        $userCorrectLogin = ['email' => $user['email'], 'password' => $user['password']];
+        $responseCorrectLogin = $this->postJson('/auth', $userCorrectLogin, $this->headers)->json();
+
+        $this->assertArrayHasKey('token', $responseCorrectLogin);
+        $this->assertArrayHasKey('user', $responseCorrectLogin);
+
+        // Testando login errado até bloquear o usuário
+        $userWrongLogin = ['email' => $user['email'], 'password' => $this->faker->text];
+
+        for ($i = 0; $i <= config('app.max_wrong_login_attempts'); $i++) {
+            $responseWrongLogin = $this->postJson('/auth', $userWrongLogin, $this->headers)->json()['message'];
+            $this->assertEquals('Usuário ou senha incorreto!', $responseWrongLogin, "Fail on attempt $i");
+        }
+
+        // Testando login com usuário bloqueado
+        $responseWrongLogin = $this->postJson('/auth', $userWrongLogin, $this->headers)->json();
+        $this->assertEquals('Usuário inativo!', $responseWrongLogin['message']);
+
+        $userDB = User::where('email', $user['email'])->first();
+        $this->assertEquals(StatusEnum::Inactive->value, $userDB->status);
+
+        // Desbloqueando usuário e testando login com sucesso
+        $this->get("/active-user/{$userDB->verify_hash}", $this->headers);
+        $responseCorrectLogin = $this->postJson('/auth', $userCorrectLogin, $this->headers)->json();
+
+        $this->assertArrayHasKey('token', $responseCorrectLogin);
+        $this->assertArrayHasKey('user', $responseCorrectLogin);
+    }
+}
