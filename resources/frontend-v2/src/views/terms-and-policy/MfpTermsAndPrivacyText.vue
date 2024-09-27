4a1d047a0746c76d6cdab59cdc13af3aaf00546b
Jhonatan Henkel <96666648+Jhon-Henkel@users.noreply.github.com>
2024-09-27 18:56:27 -0300

    terms-and-privacy-text (#813)
    * feat: added text of privacy and terms on register and on login page
    
    * feat: Create Terms Of Use
    
    * feat: Create Privacy Policy

---
+++ resources/frontend-v2/src/views/terms-and-policy/MfpTermsAndPrivacyText.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import {IonCol, IonRow, IonText} from "@ionic/vue"
+import {MfpModal} from "@/components/modal/MfpModal"
+import MfpTermsOfUseModal from "@/views/terms-and-policy/MfpTermsOfUseModal.vue"
+import MfpPrivacyPolicyModal from "@/views/terms-and-policy/MfpPrivacyPolicyModal.vue"
+
+defineProps({
+    action: {
+        type: String,
+        required: true
+    }
+})
+
+function openTermsOfUseModal() {
+    const termsModal = new MfpModal(MfpTermsOfUseModal)
+    termsModal.open()
+}
+
+function openPrivacyPolicyModal() {
+    const privacyModal = new MfpModal(MfpPrivacyPolicyModal)
+    privacyModal.open()
+}
+</script>
+
+<template>
+    <ion-row>
+        <ion-col size="1"/>
+        <ion-col size="10" class="ion-text-center">
+            <ion-text>
+                Ao se <strong>{{ action }}</strong>, você concorda com os nossos <a @click="openTermsOfUseModal">
+                Termos de Uso</a> e <a @click="openPrivacyPolicyModal">Política de Privacidade</a>.
+            </ion-text>
+        </ion-col>
+        <ion-col size="1"/>
+    </ion-row>
+</template>
