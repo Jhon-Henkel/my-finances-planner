f5fa65a294001adbf6679ce933ee2d70f55de24d
jhowrf <jhowrf@gmail.com>
2024-10-01 20:03:17 -0300

    feat: Notify master when new user is registered

--- resources/views/emails/newUserRegistered.blade.php
+++ resources/views/emails/newUserRegistered.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ativação de Conta</title>
+    <title>Novo Usuário Registrado</title>
     <style>
         body {
             font-family: Arial, sans-serif;
