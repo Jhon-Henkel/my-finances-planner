6bc92b6936411f90614138c4c4ecc8c98f856414
jhowrf <jhowrf@gmail.com>
2024-10-05 07:52:42 -0300

    feat: added filter on future profits

--- resources/frontend-v2/src/views/future-profits/FutureProfitsPage.vue
+++ resources/frontend-v2/src/views/future-profits/FutureProfitsPage.vue
@@ -2,9 +2,18 @@
 import MfpPage from "@/components/page/MfpPage.vue"
 import MfpCirclePlusButton from "@/components/button/MfpCirclePlusButton.vue"
 import MfpRefresh from "@/components/refresh/MfpRefresh.vue"
-import {IonIcon, IonItemOption, IonItemOptions, IonItemSliding, IonLabel, IonList, IonListHeader} from "@ionic/vue"
+import {
+    IonButton,
+    IonIcon,
+    IonItemOption,
+    IonItemOptions,
+    IonItemSliding,
+    IonLabel,
+    IonList,
+    IonListHeader
+} from "@ionic/vue"
 import {useFutureProfitsStore} from "@/stores/future-profits/FutureProfitsStore"
-import {onMounted} from "vue"
+import {onMounted, ref} from "vue"
 import MfpPeriodSwitcher from "@/components/switcher/MfpPeriodSwitcher.vue"
 import MfpTotalRegistersRow from "@/components/page/MfpTotalRegistersRow.vue"
 import MfpFutureProfitsDetailsCard from "@/views/future-profits/MfpFutureProfitsDetailsCard.vue"
@@ -23,10 +32,12 @@ import MfpInvoiceDetailsModal from "@/views/invoice/MfpInvoiceDetailsModal.vue"
 import {InvoiceService} from "@/services/invoice/InvoiceService"
 import {MfpOkAlert} from "@/components/alert/MfpOkAlert"
 import MfpFutureProfitsReceiveModal from "@/views/future-profits/MfpFutureProfitsReceiveModal.vue"
+import {InvoiceModel} from "@/model/invoice/invoiceModel"
 
 const store = useFutureProfitsStore()
 const formModal = new MfpModal(MfpFutureProfitsFormModal)
 const okAlert = new MfpOkAlert('Ação inválida!')
+const onlyNonReceived = ref(true)
 
 async function optionsAction(item: IInvoice) {
     const actionSheet = new MfpActionSheet(UtilActionSheet.makeButtonsToFutureProfits())
@@ -51,6 +62,10 @@ async function optionsAction(item: IInvoice) {
     }
 }
 
+function mustShowItem(item: InvoiceModel, onlyNonReceived: boolean): boolean {
+    return ! onlyNonReceived || (onlyNonReceived && InvoiceService.getInvoiceValueByNumber(store.installmentSelected, item) > 0)
+}
+
 async function handleRefresh(event: any) {
     await FutureProfitsService.forceLoadStore()
     event.target.complete()
@@ -72,10 +87,19 @@ onMounted(async () => {
         </ion-list-header>
         <mfp-period-switcher :store="store"/>
         <mfp-future-profits-details-card/>
+        <div class="ion-text-end">
+            <ion-button fill="clear" @click="onlyNonReceived = !onlyNonReceived">
+                {{ onlyNonReceived ? 'Ver Todas' : 'Ver somente a receber' }}
+            </ion-button>
+        </div>
         <mfp-empty-list-item :nothing-to-show="store.futureProfits.length === 0 && store.isLoaded"/>
         <mfp-invoice-list-skeleton-load :is-loaded="store.isLoaded"/>
         <ion-list v-if="store.isLoaded">
-            <ion-item-sliding v-for="(item, index) in store.futureProfits" :key="index">
+            <ion-item-sliding
+                v-for="(item, index) in store.futureProfits"
+                :key="index"
+                v-show="mustShowItem(item, onlyNonReceived)"
+            >
                 <mfp-invoice-list-item :store="store" :invoice-item="item" fix-installment-label="Receita"/>
                 <ion-item-options side="end">
                     <ion-item-option color="light" @click="optionsAction(item)">
