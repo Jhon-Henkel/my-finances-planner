073044dce9c8a605916955b2155f23633b4dfba7
Jhonatan Henkel <96666648+Jhon-Henkel@users.noreply.github.com>
2024-10-05 07:36:40 -0300

    feat: Added description for movements in dashboard (#848)

--- resources/frontend-v2/src/util/UtilCalendar.ts
+++ resources/frontend-v2/src/util/UtilCalendar.ts
@@ -17,6 +17,10 @@ export const UtilCalendar = {
         const dateTime: Date = new Date(date)
         return dateTime.toLocaleDateString('pt-BR', { year: '2-digit', month: '2-digit', day: '2-digit' })
     },
+    formatStringToBrOnlyDayAndMonth: function(date: string): string {
+        const dateTime: Date = new Date(date)
+        return dateTime.toLocaleDateString('pt-BR', { year: undefined, month: '2-digit', day: '2-digit' })
+    },
     getTodayIso: function(): string {
         return this.getToday().toISOString()
     },
