1cf391a198aff49cf924ec0803623a840c47d865
jhowrf <jhowrf@gmail.com>
2024-10-11 07:31:39 -0300

    feat: configure exceptions on php stan, fix redis connection

--- app/Tools/Cache/MfpCacheManagerReal.php
+++ app/Tools/Cache/MfpCacheManagerReal.php
@@ -4,26 +4,36 @@ namespace App\Tools\Cache;
 
 use App\Enums\Cache\CacheKeyEnum;
 use App\Enums\TimeNumberEnum;
-use App\Models\User;
-use Illuminate\Support\Facades\Redis;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Redis;
 
 class MfpCacheManagerReal
 {
-    public function setModel(string $email, CacheKeyEnum $key, Model $model, TimeNumberEnum $expires = TimeNumberEnum::ThreeHourInSeconds): void
-    {
+    protected const string EXPIRES_TYPE_SECONDS = 'EX';
+
+    public function setModel(
+        string $email,
+        CacheKeyEnum $key,
+        Model $model,
+        TimeNumberEnum $expires = TimeNumberEnum::ThreeHourInSeconds
+    ): void {
         if (config('app.use_redis') === false) {
             return;
         }
-        Redis::set(md5($email) . $key->value, serialize($model), $expires->value);
+        Redis::set($this->makeKey($email, $key), serialize($model), self::EXPIRES_TYPE_SECONDS, $expires->value);
     }
 
-    public function getModel(string $email, CacheKeyEnum $key): null|Model|User
+    public function getModel(string $email, CacheKeyEnum $key): null|Model
     {
         if (config('app.use_redis') === false) {
             return null;
         }
-        $data = Redis::get(md5($email) . $key->value);
+        $data = Redis::get($this->makeKey($email, $key));
         return $data ? unserialize($data) : null;
+    }
+
+    protected function makeKey(string $email, CacheKeyEnum $key): string
+    {
+        return md5($email) . $key->value;
     }
 }
