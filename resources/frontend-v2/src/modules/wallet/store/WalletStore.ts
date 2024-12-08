8eee3216dd25373fd16ec3ce0ccb3294271a1b10
jhowrf <jhowrf@gmail.com>
2024-12-08 12:51:19 -0300

    feat: update wallet list

--- resources/frontend-v2/src/modules/wallet/store/WalletStore.ts
+++ resources/frontend-v2/src/modules/wallet/store/WalletStore.ts
@@ -2,12 +2,24 @@ import { defineStore } from 'pinia'
 import {WalletModel} from "@/modules/wallet/model/WalletModel"
 import {WalletService} from "@/modules/wallet/service/WalletService"
 
+interface IStateWallet {
+    wallets: Array<WalletModel>,
+    hiddenWallets: Array<WalletModel>,
+    notHiddenWallets: Array<WalletModel>,
+    isLoaded: boolean,
+    totalAmount: number,
+    totalAmountHidden: number
+}
+
 export const useWalletStore = defineStore({
     id: 'wallet',
-    state: (): {wallets: Array<WalletModel>, isLoaded: boolean, totalAmount: number} => ({
+    state: (): IStateWallet => ({
         wallets: [],
+        hiddenWallets: [],
+        notHiddenWallets: [],
         isLoaded: false,
-        totalAmount: 0
+        totalAmount: 0,
+        totalAmountHidden: 0
     }),
     actions: {
         loadAgainOnNextTick() {
@@ -20,6 +32,14 @@ export const useWalletStore = defineStore({
             if (!this.isLoaded) {
                 this.isLoaded = false
                 this.wallets = await WalletService.index()
+                for (let i = 0; i < this.wallets.length; i++) {
+                    if (this.wallets[i].hideValue) {
+                        this.hiddenWallets.push(this.wallets[i])
+                        this.totalAmountHidden += this.wallets[i].amount
+                    } else {
+                        this.notHiddenWallets.push(this.wallets[i])
+                    }
+                }
                 this.isLoaded = true
             }
             return this.wallets
