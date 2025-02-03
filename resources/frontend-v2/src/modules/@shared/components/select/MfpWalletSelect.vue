e16cc5803db218254871aa560f48cd8e2c0a92cb
Jhonatan Henkel <96666648+Jhon-Henkel@users.noreply.github.com>
2025-02-03 10:27:16 -0300

    Update MfpWalletSelect.vue

--- resources/frontend-v2/src/modules/@shared/components/select/MfpWalletSelect.vue
+++ resources/frontend-v2/src/modules/@shared/components/select/MfpWalletSelect.vue
@@ -41,7 +41,7 @@ onMounted(async () => {
             :value="modelValue"
             interface="action-sheet"
             cancel-text="Cancelar"
-            @ionChange="updateValue($event.target.value)"
+            @ionChange="updateValue($event.detail.value)"
         >
             <ion-item v-for="(wallet, index) in walletStore.activeWallets" :key="index">
                 <ion-select-option :value="wallet.id">
