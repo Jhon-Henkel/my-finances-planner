01e5947a72a0d8df3e26b73c57d000bc6304a007
Jhonatan Henkel <96666648+Jhon-Henkel@users.noreply.github.com>
2025-02-08 22:12:21 -0300

    corrigir-estrutura-do-ccoverage (#1003)
    * test(coverage) Melhorando a estrutura do coverage
    
    * test: refatorando teste de registro de usuário
    
    * test: refatorando teste de registro e bloqueio de usuário
    
    * test: refatorando teste de registro e bloqueio de usuário
    
    * test: refatorando teste de registro e bloqueio de usuário
    
    * test: refatorando teste de registro e bloqueio de usuário
    
    * test: refatorando teste de registro e bloqueio de usuário

--- tests/backend/Feature/UseCases/Plan/Free/FreePlanFinancialHealthLimitationUseCase.php
+++ tests/backend/Feature/UseCases/Plan/Free/FreePlanFinancialHealthLimitationUseCase.php
@@ -2,15 +2,15 @@
 
 namespace Tests\backend\Feature\UseCases\Plan\Free;
 
-use Tests\backend\Falcon9Feature;
+use Tests\backend\Falcon9FeatureWithTenantDatabase;
 
-class FreePlanFinancialHealthLimitationUseCase extends Falcon9Feature
+class FreePlanFinancialHealthLimitationUseCase extends Falcon9FeatureWithTenantDatabase
 {
     private string $baseUrl = '/api/financial-health/filter';
 
     public function testFinancialHealthFreePlanLimitation()
     {
-        $response = $this->getJson($this->baseUrl, $this->makeHeaders());
+        $response = $this->getJson($this->baseUrl, $this->makeApiHeaders());
         $response->assertStatus(403);
         $response->assertJson([
             'message' => 'Limite de \'saúde financeira\' atingido para o seu plano.',
